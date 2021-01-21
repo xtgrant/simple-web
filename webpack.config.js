509379d4dd6ef34cec9e673aa9ed3ad37c41aec9
Nathan Melton <mlgmainstream@gmail.com>
2021-01-21 15:42:50 -0800

    stable dev server with html and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
@@ -17,10 +19,29 @@ module.exports = {
     hot: true,
     port: 8080,
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'src')],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
   plugins: [
     /* ... */
     // Only update what has changed on hot reload
     new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    })
   ],
 }
